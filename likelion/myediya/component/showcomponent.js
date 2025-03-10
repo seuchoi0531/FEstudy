b2fb32fbb25a9f9107a5932f12d428b68ca0ac0b
seuchoi0531 <seuchoi0531@gmail.com>
2025-03-10 15:00:15 +0900

    remove h1 button in component, add prebutton

--- likelion/myediya/component/showcomponent.js
+++ likelion/myediya/component/showcomponent.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelector(".component-name")
+    .addEventListener("click", scrollToBottom);
+});
+
 function scrollToBottom() {
   window.scrollTo({
     top: document.documentElement.scrollHeight,
